c0583ba82787476f0d5dbc8956c3a5f6fd7632ef
Burak-Esen <0.burak.esen@gmail.com>
2020-07-21 20:44:30 +0300

    calculator

--- main.py
+++ main.py
@@ -1,29 +1,58 @@
-class Dog:
-    sound = "bark"
+class Calculator:
+    def __init__(self, a, b):
+        self.history=[]
+        self.a = a
+        self.b = b
 
-    def speak(self):
-        print(Dog.sound)
+    def add(self):
+        self.history.append("+")
+        return self.a + self.b
+
+    def substrctn(self):
+        self.history.append("-")
+        return self.a - self.b
+
+    def multiplctn(self):
+        self.history.append("*")
+        return self.a * self.b
+
+    def division(self):
+        self.history.append("/")
+        return self.a / self.b
+    def printHistory(self):
+        print(self.history)
+    def clearHistory(self):
+        self.history.clear()
+    def startCalculator(self):
+        choice = 1
+        while choice != 0:
+            print("0. Exit")
+            print("1. Add")
+            print("2. Substraction")
+            print("3. Multiplication")
+            print("4. Division")
+            choice = int(input("Enter choice:"))
+            if choice == 1:
+                print("Result: ", self.add())
+            elif choice == 2:
+                print("Result: ", self.substrctn())
+            elif choice == 3:
+                print("Result: ", self.multiplctn())
+            elif choice == 4:
+                print("Result: ", self.division())
+            elif choice == 0:
+                print("Exiting! ")
+            else:
+                print("Invalid Choice !")
 
 
-class Cat:
-    def __init__(self):
-        self.sound = "Meeuw"
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
 
-    def speak(self):
-        print(self.sound)
+obj = Calculator(a, b)
 
+obj.startCalculator()
 
-d1 = Dog()
-c1 = Cat()
-
-c1.speak()
-d1.speak()
-
-c1.name = "Katz"
-print(c1.name)
-print(hasattr(c1, "surname"))
-print(getattr(c1, "name"))
-print(setattr(c1, "name", "Gerald"))
-print(getattr(c1, "name"))
-print(delattr(c1, "name"))
-print(hasattr(c1, "name"))
+obj.printHistory()
+obj.clearHistory()
+obj.printHistory()
